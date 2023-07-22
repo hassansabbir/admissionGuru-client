64eb63c52a1120291ffec22a86b0240b6b9c900d
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-07-22 23:58:34 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,19 +2,19 @@ import bannerImg from "./../../../assets/29808764_7606072.jpg";
 
 const Banner = () => {
   return (
-    <div className="flex justify-between">
-      <div>
+    <div className="flex items-center justify-between h-[550px]">
+      <div className="">
         <h2 className="text-5xl ">AdmissionGuru</h2>
-        <p className="text-2xl">
+        <p className="text-2xl my-5">
           Discover your dream college at AdmissionGuru! 🎓 <br /> Explore
           prestigious universities and institutions, compare details, and make
           an informed decision. Unleash your potential and find the perfect
           match for your future. Join us on this exciting educational journey.
           Your dream college awaits.
         </p>
-        <p className="text-2xl">Start exploring now! 🚀🌟</p>
+        <p className="text-2xl my-5">Start exploring now! 🚀🌟</p>
       </div>
-      <img className="w-4/12" src={bannerImg} alt="bannerImg" />
+      <img className="w-5/12" src={bannerImg} alt="bannerImg" />
     </div>
   );
 };
