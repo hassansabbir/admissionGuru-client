090e98ab32c424e022d917b33bdd99ef490ed6a5
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-07-24 13:02:03 +0600

    dynamic rating & feedback system added

--- src/Pages/MyCollege/MyCollege.jsx
+++ src/Pages/MyCollege/MyCollege.jsx
@@ -1,21 +1,12 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Reviews from "./Reviews/Reviews";
+import useProfile from "../../hooks/useProfile";
 
 const MyCollege = () => {
   const { user } = useContext(AuthContext);
 
-  const [myCollege, setMyCollege] = useState([]);
-  useEffect(() => {
-    fetch(`http://localhost:5000/allBookings/${user?.email}`, {
-      method: "GET",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(),
-    })
-      .then((res) => res.json())
-      .then((data) => setMyCollege(data.collegeData));
-  }, [user]);
-  console.log(myCollege);
+  const [myCollege] = useProfile();
 
   return (
     <div className="py-24">
@@ -23,22 +14,22 @@ const MyCollege = () => {
       <div>
         <img
           className="w-full rounded-3xl md:h-[750px] "
-          src={myCollege.collegeImage}
+          src={myCollege?.collegeData?.collegeImage}
           alt="myCollege"
         />
         <h2 className="text-4xl text-center font-bold mt-10">
-          {myCollege.collegeName}
+          {myCollege?.collegeData?.collegeName}
         </h2>
         <p className="text-xl mx-16 my-10">
           <span className="font-bold text-blue-800 text-2xl">History: </span>{" "}
           <br />
-          {myCollege.researchHistory}
+          {myCollege?.collegeData?.researchHistory}
         </p>
       </div>
       <div className="px-5 md:px-0">
         <h2 className="text-4xl my-10 font-bold">Popular Sports:</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-          {myCollege?.sports?.map((sport, i) => (
+          {myCollege?.collegeData?.sports?.map((sport, i) => (
             <div
               className="bg-slate-100 p-10 shadow-lg hover:shadow-2xl rounded-3xl"
               key={i}
@@ -60,7 +51,7 @@ const MyCollege = () => {
       <div className="px-5 md:px-0">
         <h2 className="text-4xl my-10 font-bold">Our Events:</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-          {myCollege?.events?.map((event, i) => (
+          {myCollege?.collegeData?.events?.map((event, i) => (
             <div
               className="bg-slate-200 p-10 shadow-lg hover:shadow-2xl rounded-3xl"
               key={i}
@@ -77,7 +68,7 @@ const MyCollege = () => {
       <div>
         <h2 className="text-5xl text-center font-bold my-16">Review</h2>
       </div>
-      <Reviews />
+      <Reviews user={user} myCollege={myCollege?.collegeData} />
     </div>
   );
 };
--- src/Pages/MyCollege/Reviews/Reviews.jsx
+++ src/Pages/MyCollege/Reviews/Reviews.jsx
@@ -1,9 +1,107 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { FaStar } from "react-icons/fa";
+import Swal from "sweetalert2";
 
-const Reviews = () => {
+const Reviews = ({ user, myCollege }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(null);
+
+  const {
+    register,
+    handleSubmit,
+
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const addAReview = {
+      name: user?.displayName,
+      email: user?.email,
+      photoUrl: user?.photoURL,
+      ratedCollege: myCollege?.collegeName,
+      ratedCollegeId: myCollege?._id,
+      rating: parseFloat(rating),
+      ...data,
+    };
+    fetch(`${import.meta.env.VITE_SERVER_API}/reviews`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(addAReview),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success",
+            text: `Successfully rating added to ${myCollege.collegeName}`,
+            icon: "success",
+            confirmButtonText: "Done",
+          });
+        }
+        reset();
+      });
+  };
+
   return (
-    <div>
-      <FaStar size={50} />
+    <div className="flex flex-col py-20 bg-slate-100 rounded-3xl justify-center items-center">
+      <div className="p-5 border shadow-md bg-base-100 hover:shadow-2xl w-5/12 rounded-3xl my-5">
+        <h2 className="text-3xl my-5 font-bold">
+          Give Your Rating on This College.
+        </h2>
+        <div className="flex items-center gap-10">
+          <div className="flex">
+            {[...Array(5)].map((star, index) => {
+              const currentRating = index + 1;
+              return (
+                <label key={index}>
+                  <input
+                    className="hidden"
+                    type="radio"
+                    name="rating"
+                    value={currentRating}
+                    onClick={() => setRating(currentRating)}
+                  />
+                  <FaStar
+                    className=" cursor-pointer"
+                    size={50}
+                    color={
+                      currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"
+                    }
+                    onMouseEnter={() => setHover(currentRating)}
+                    onMouseLeave={() => setHover(null)}
+                  />
+                </label>
+              );
+            })}
+          </div>
+          <p className="text-3xl">
+            Your rating is - <span className="font-bold">{rating}/5</span>
+          </p>
+        </div>
+      </div>
+      <div className="border p-5 text-center bg-base-100 shadow-md hover:shadow-2xl rounded-3xl w-6/12">
+        <h2 className="text-3xl my-5 font-bold">
+          Give Your Feedback on This College.
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <textarea
+            {...register("feedback")}
+            className="border p-5 text-xl"
+            name="feedback"
+            placeholder="Give your feedback here"
+            cols="60"
+            rows="5"
+          ></textarea>
+          <button
+            className="btn bg-blue-800 hover:bg-blue-600 text-white"
+            type="submit"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
